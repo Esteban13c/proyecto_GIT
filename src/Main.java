1622807bb7e038fe44eb767e94ac4368c8c2f2e9
Esteban13c <juanesteban2727cruz@gmail.com>
2026-08-20 22:08:09 -0500

    Agregando manual en PDF

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
-    System.out.println("Probando comandos de Git");
+    System.out.println(String.format("Hola a todos!"));
 
 
 }
